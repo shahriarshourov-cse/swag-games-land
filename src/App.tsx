56bdcff1e4646d4dfdb687599b90d2ef1859cd47
Shahriar Shourov <shahriarshourov@github.com>
2023-09-12 11:20:53 +0600

    Navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem } from "@chakra-ui/react";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
@@ -9,8 +10,8 @@ function App() {
           lg: `"nav nav" "aside main"`,
         }}
       >
-        <GridItem area="nav" bg="coral">
-          Nav
+        <GridItem area="nav">
+          <Navbar></Navbar>
         </GridItem>
         <GridItem area="aside" bg="gold">
           Aside
